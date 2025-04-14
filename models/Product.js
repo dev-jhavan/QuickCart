3bd83e41249550f8775e34e15f80770d41bfe205
dev-jhavan <jhavanmaciel10@gmail.com>
2025-04-14 17:32:20 -0300

    kkkkkkkk

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Scheema({
+const productSchema = new mongoose.Schema({
     userId: {type: String, required:true, ref: "user"},
     name: {type: String, required:true},
     description: {type: String, required:true},
