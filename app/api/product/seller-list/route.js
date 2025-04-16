772c6d624811a8491621967e9a57cb923337c929
dev-jhavan <jhavanmaciel10@gmail.com>
2025-04-16 18:53:57 -0300

    atualização dos itens adicionados na lista

---
+++ app/api/product/seller-list/route.js
@@ -0,0 +1,23 @@
+import connectDB from '@/config/db';
+import authSeller from '@/lib/authSeller';
+import Product from '@/models/Product';
+import { NextResponse } from 'next/server';
+import { getAuth } from '@clerk/nextjs/server';
+
+
+export async function GET(request) {
+    try {
+        const { userId } = getAuth(request)
+
+        const inSeller = authSeller(userId)
+        if (!isSeller) {
+            return NextResponse.json({ success: false, message: 'not authorized' });
+
+        }
+        await connectDB()
+        const products = await Product.find({})
+        return NextResponse.json({ success: true, products })
+    } catch (error) {
+        return NextResponse.json({success: false, message: error.message })
+    }
+}
